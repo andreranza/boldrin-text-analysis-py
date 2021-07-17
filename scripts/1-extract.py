5ff6e96f36c37f24a7549f1cc31b739edcb4f546
Andrea Ranzato <andrea.ranzato01@gmail.com>
2021-07-17 21:38:34 +0200

    Handle failures in data input

--- scripts/1-extract.py
+++ scripts/1-extract.py
@@ -3,24 +3,51 @@ import json
 import configparser
 import boto3
 import os
+import logging
 import pandas as pd
 from datetime import datetime
 from googleapiclient.discovery import build
 
-today = datetime.today()
-yt_key = os.environ.get('YT_API_PSW')
+today = datetime.date(datetime.today())
+
+# logging configurations
+logging.basicConfig(
+    filename='logs/elt-youtube-api-log_{0}.txt'.format(str(today)),
+    level=logging.DEBUG, format=' %(asctime)s - %(levelname) s - %(message)s'
+)
+
+# retrieve API credentials
+try:
+    #yt_key = os.environ.get('YT_API_PSW')
+    with open('yt-api-psw.txt') as f:
+        yt_key = f.read().rstrip()
+except FileNotFoundError:
+    logging.warning('Not able to API credentials id locally')
+    yt_key = str(input('Please, provide the API service key: ')).rstrip()
+
+
+# retrieve channel id
+try:
+    with open('channel_id.txt') as f:
+        CH_ID = f.read().rstrip()
+except FileNotFoundError:
+    logging.warning('Not able to retrieve channel id')
+    CH_ID = str(input('Please, provide the channel id: ')).rstrip()
+
 api_service_name = 'youtube'
 api_version = 'v3'
-CH_ID = 'UCMOiTfbUXxUFqJJtCQGHrrA'
 
 # call the service
 youtube = build(serviceName=api_service_name, version=api_version, developerKey=yt_key)
+logging.info('Called the YouTube API Service')
 
 # id pointing to the playlist of uploaded video
 upload_id = yt.get_upload_id(service_obj=youtube, channel_id=CH_ID)
+logging.info('Got the upload id')
 
 # list of id, one for each uploaded video in a given channel
 video_id = yt.get_video_id(service_obj=youtube, upload_id=upload_id)
+logging.info('Retrieved list of videos id')
 
 # retrieve data records from the 'items' node of json response recursively
 
@@ -57,15 +84,15 @@ for i, id in enumerate(video_id):
 videos_df = pd.concat(dataframes)
 
 # add timestams
-video_df = video_df.assign(timestamp=datetime.now())
+videos_df = videos_df.assign(timestamp=datetime.now())
 
 # extract channel name
-ch_name = df.loc[1, 'channelTitle']
-export_file = 'data/{0}_{1}.csv'.format(str(datetime.date(today)), ch_name).replace(' ', '')
+ch_name = videos_df.loc[1, 'channelTitle']
+export_file = 'data/{0}_{1}.csv'.format(str(today), ch_name).replace(' ', '')
 
 # save file
 videos_df.to_csv(export_file, index=False)
-
+logging.info('Saved output locally in {0}'.format(export_file))
 
 # load credentials
 parser = configparser.ConfigParser()
@@ -79,6 +106,10 @@ s3 = boto3.client(
     aws_access_key_id=access_key,
     aws_secret_access_key=secret_key
 )
+logging.info('Connected to S3 bucket: {0}'.format(bucket_name))
 
 # upload file
 s3.upload_file(export_file, bucket_name, export_file)
+logging.info('Uploaded output to S3 bucket: {0}'.format(bucket_name))
+
+logging.info('Data extraction concluded')
