7b4912c99fbce2037dd89e9681a7817ff1d4c715
Andrea Ranzato <andrea.ranzato01@gmail.com>
2023-06-04 01:55:18 +0200

    Should not be a const

--- pyelt/scripts/1-extract-id.py
+++ pyelt/scripts/1-extract-id.py
@@ -21,21 +21,21 @@ except FileNotFoundError:
     logging.warning("Not able to retrieve channel id")
     CH_ID = str(input("Please, provide the channel id: ")).rstrip()
 
-api_service_name = "youtube"
-api_version = "v3"
+yt_key = read_yt_key(path="infra/youtube_api_key.txt")
+youtube_service = yt.call_api(yt_key, "youtube", "v3")
 
 # id pointing to the playlist of uploaded video
-upload_id = yt.get_upload_id(service_obj=youtube, channel_id=CH_ID)
+upload_id = yt.get_upload_id(service_obj=youtube_service, channel_id=CH_ID)
 logging.info("Got the upload id")
 
 # list of id, one for each uploaded video in a given channel
-video_id = yt.get_video_id(service_obj=youtube, upload_id=upload_id)
+video_id = yt.get_video_id(service_obj=youtube_service, upload_id=upload_id)
 logging.info("Retrieved list of videos id")
 
 # iterate through the ids to get a json response
 for id in enumerate(video_id):
     # get response in JSON format
-    video_json = yt.get_response(service_obj=youtube, video_id=id)
+    video_json = yt.get_response(service_obj=youtube_service, video_id=id)
 
 s3 = boto3.client("s3", aws_access_key_id=access_key, aws_secret_access_key=secret_key)
 logging.info("Connected to S3 bucket: {0}".format(bucket_name))
